7cfe7709da8f0bcd2bd9a97ad3357c920f0a302e
Yuliya <yuliya.rostova@gmail.com>
2021-09-20 03:05:08 +0300

    feat: solve what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason( date ) {
+  // throw new NotImplementedError('Not implemented');
+  if (!date) return 'Unable to determine the time of year!';
+
+  try {
+    date.getUTCFullYear();
+  } catch (e) {
+    throw new Error("Invalid date!");
+  }
+
+  if (date.getMonth() == 0) return 'winter';
+  if (date.getMonth() == 1) return 'winter';
+  if (date.getMonth() == 2) return 'spring';
+  if (date.getMonth() == 3) return 'spring';
+  if (date.getMonth() == 4) return 'spring';
+  if (date.getMonth() == 5) return 'summer';
+  if (date.getMonth() == 6) return 'summer';
+  if (date.getMonth() == 7) return 'summer';
+  if (date.getMonth() == 8) return 'autumn';
+  if (date.getMonth() == 9) return 'autumn';
+  if (date.getMonth() == 10) return 'autumn';
+  if (date.getMonth() == 11) return 'winter';
+
+  return 'Invalid date!';
 }
